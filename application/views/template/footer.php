117548b15dcf8ca858142d3313225c2c5c1d1667
hujianglu <269018634@qq.com>
2015-10-20 00:00:24 +0800

    包含的尾文件

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,17 +1,27 @@
-            </div>
-        </div>
-    </div>
-    <footer>
-        <div class="container">
-            <div class="row">
-                <div class="span6">&copy; Copyright 2015</div>
-                <div class="span6"></div>
-            </div>
-        </div>
-    </footer>
-<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<base href="<?php echo base_url();?>" />
+
+<!-- basic scripts -->
 <script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
-<!-- Include all compiled plugins (below), or include individual files as needed -->
-<script src="<?php echo base_url('js/bootstrap.min.js');?>"></script>
+<!--[if !IE]> -->
+<script type="text/javascript">
+    window.jQuery || document.write("<script src='assets/js/jquery-2.0.3.min.js'>"+"<"+"/script>");
+</script>
+<!-- <![endif]-->
+<!--[if IE]>
+<script type="text/javascript">
+    window.jQuery || document.write("<script src='assets/js/jquery-1.10.2.min.js'>"+"<"+"/script>");
+</script>
+<![endif]-->
+<script type="text/javascript">
+    if("ontouchend" in document) document.write("<script src='assets/js/jquery.mobile.custom.min.js'>"+"<"+"/script>");
+</script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/typeahead-bs2.min.js"></script>
+<!-- page specific plugin scripts -->
+<!-- ace scripts -->
+<script src="assets/js/ace-elements.min.js"></script>
+<script src="assets/js/ace.min.js"></script>
+<!-- inline scripts related to this page -->
+<!--<div style="display:none"><script src='http://v7.cnzz.com/stat.php?id=155540&web_id=155540' language='JavaScript' charset='gb2312'></script></div>-->
 </body>
 </html>
